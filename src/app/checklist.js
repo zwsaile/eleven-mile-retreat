8a22adb07de1de3972797f172856cf2dcbc469b0
Zachary Saile <zwsaile@gmail.com>
2024-07-20 19:07:34 -0700

    Change sizes for responsiveness

--- src/app/checklist.js
+++ src/app/checklist.js
@@ -1,13 +1,13 @@
 export default function Checklist() {
     return (
       <div className="relative h-screen snap-start flex flex-col justify-center items-center px-4">
-        <div className="w-full max-w-4xl bg-[rgba(255,255,255,0.1)] backdrop-blur-md rounded-lg p-8 mt-10 shadow-md">
-          <h2 className="text-2xl font-bold text-center text-white mb-6">Camping Trip Checklist</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="w-full max-w-4xl max-h-[80%] bg-[rgba(255,255,255,0.1)] backdrop-blur-md rounded-lg p-8 mt-10 shadow-md">
+          <h2 className="text-2xl font-bold text-center text-white mb-5">Camping Trip Checklist</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {/* Must Bring Items */}
             <div>
-              <h3 className="text-xl font-semibold text-white mb-4">Must Bring Supplies</h3>
-              <ul className="list-disc list-inside text-white">
+              <h3 className="text-lg font-semibold text-white mb-4">Must Bring Supplies</h3>
+              <ul className="list-disc list-inside text-white text-sm">
                 <li>Tent</li>
                 <li>Sleeping Bag</li>
                 <li>Backpack</li>
@@ -19,8 +19,8 @@ export default function Checklist() {
             </div>
             {/* Nice to Have Items */}
             <div>
-              <h3 className="text-xl font-semibold text-white mb-4">Nice to Have Supplies</h3>
-              <ul className="list-disc list-inside text-white">
+              <h3 className="text-lg font-semibold text-white mb-4">Nice to Have Supplies</h3>
+              <ul className="list-disc list-inside text-white text-sm">
                 <li>Sleeping Pad</li>
                 <li>Camping Chair</li>
                 <li>Book or Journal</li>
@@ -31,7 +31,7 @@ export default function Checklist() {
               </ul>
             </div>
           </div>
-          <p className="text-md text-justify font-light text-white mt-10 mb-3">Note: we may be able to arrange for certain must bring supplies to be loaned to those in need.</p>
+          <p className="text-xs text-justify font-light text-white mt-10 mb-3">Note: we may be able to arrange for certain must bring supplies to be loaned to those in need.</p>
         </div>
       </div>
     );
