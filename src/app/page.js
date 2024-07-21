e80685616698302169f245e7f68fdc6decd46478
Zachary Saile <zwsaile@gmail.com>
2024-07-20 18:49:15 -0700

    Fix typo

--- src/app/page.js
+++ src/app/page.js
@@ -24,10 +24,10 @@ export default function Page() {
           <div className="h-screen snap-start relative flex flex-col justify-end items-center">
             <div className="absolute top-[20%] w-[80%] flex flex-col justify-center items-center bg-[rgba(255,255,255,0.2)] backdrop-blur-sm p-6 shadow-md rounded-md">
               <h1 className="text-center text-3xl font-bold mb-5 text-white">
-                "Most of us feel we need look no further for Utopia.
+                Most of us feel we need look no further for Utopia.
               </h1>
               <h2 className="text-center text-2xl font-normal text-white">
-                We have it with us right here and now."
+                We have it with us right here and now.
               </h2>
             </div>
             <div className="flex flex-row justify-around w-full max-w-4xl bg-[rgba(255,255,255,0.1)] backdrop-blur-sm rounded-lg p-8 m-4 text-center">
